1c4e789e5a0f993b71f190f09130e17ae454226e
hy <hooiyan.hoe@atoz-software.tech>
2023-04-19 22:20:11 +0800

    ignore .env.local & update VITE_APP_NAME

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ function App() {
 	return (
 		<div className='bg-gradient-to-b from-blue-600 to-violet-600 text-white flex items-center justify-center h-screen text-3xl'>
 			<p>{import.meta.env.VITE_APP_NAME}</p>
-			<p>({import.meta.env.MODE})</p>
 		</div>
 	);
 }
