7b1d7f008f963467879ffc9f4e9a5057bb8bcd58
hy <hooiyan.hoe@atoz-software.tech>
2023-04-20 18:01:24 +0800

    make alias import work

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from 'src/App';
+import 'src/styles/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />,
+			children: [
+				{
+					// path: "team",
+					// element: <Team />,
+					// loader: teamLoader,
+				},
+			],
+		},
+	],
+	{
+		future: { v7_normalizeFormMethod: true },
+	},
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 );
