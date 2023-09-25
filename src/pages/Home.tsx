ede64d65a0b6e733346ca674f40ed859684be428
hy <hooiyan.hoe@atoz-software.tech>
2023-09-25 13:29:14 +0800

    feat: 💄 add custom font & update prettier config

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import viteLogo from '/images/vite.svg';
 
 const Home: React.FC = () => {
 	return (
-		<div className='bg-gradient-to-r from-blue-600 to-violet-600 text-white items-center justify-center h-screen text-3xl flex'>
+		<div className='flex h-screen items-center justify-center bg-gradient-to-r from-blue-600 to-violet-600 text-3xl font-medium text-white'>
 			<img src={viteLogo} alt='vite logo' />
 			<p>{import.meta.env.VITE_APP_NAME}</p>
 			<p>{import.meta.env.MODE}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ export default {
 	},
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			// Add custom font here
+			fontFamily: {
+				roboto: ['Roboto', 'sans-serif'],
+				poppins: ['Poppins', 'sans-serif'],
+				montserrat: ['Montserrat', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [require('tailwindcss-touch')()],
 };
